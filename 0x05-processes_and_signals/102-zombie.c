112e6c7dbe3bceb068af3086f6cf6bcbf299c12a
6godpro <erickson5115@gmail.com>
2023-05-02 16:07:57 +0100

    initial upload task 102

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+
+/**
+ * infinite_while - Loops indefinitely, pauses for one second between
+ *                  each iteration.
+ */
+void infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+}
+
+/**
+ * main - Entry point to the program.
+ * Return: Always 0
+ */
+int main(void)
+{
+	pid_t pid;
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		pid = fork();
+		if (pid < 0)
+			exit(EXIT_FAILURE);
+		else if (pid == 0)
+			exit(EXIT_SUCCESS);
+		else
+			printf("Zombie process created, PID: %d\n", pid);
+	}
+
+	infinite_while();
+
+	return (0);
+}

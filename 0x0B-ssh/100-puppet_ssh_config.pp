0aad4bf95b9df193ba11dcd7e3845de03b739065
6godpro <erickson5115@gmail.com>
2023-08-09 17:24:16 +0100

    initial upload task 0

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,11 @@
 # Set up the SSH config file.
-include stdlib
-file_line {'Turn off passwd auth':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication no',
+import stdlib
+file_line { 'Turn off passwd auth':
+  line   => 'PasswordAuthentication yes',
   match  => 'PasswordAuth',
-}
-
-file_line {'Declare identity file':
-  ensure => present,
   path   => '/etc/ssh/ssh_config',
+}
+file_line { 'Declare identity file':
   line   => 'IdentityFile ~/.ssh/school',
+  path   => '/etc/ssh/ssh_config',
 }

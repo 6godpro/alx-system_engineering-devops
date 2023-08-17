7bea90cfa531799fb84afa8362060734c3d82e6e
6godpro <erickson5115@gmail.com>
2023-08-17 18:56:34 +0100

    re-upload task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # Re-configures the ULIMIT  NGINX
 exec { 'fix--for-nginx':
   provider => 'shell',
-  command  => "sed -i 's.-n 15.-n 4096.g' /etc/default/nginx",
+  command  => "sed -i 's:ULIMIT=\"-n 15\"::g' /etc/default/nginx",
 }

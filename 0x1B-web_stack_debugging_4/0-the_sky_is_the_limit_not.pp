37d7143efcbbf30859214e871d681f7647c63627
6godpro <erickson5115@gmail.com>
2023-08-17 19:00:08 +0100

    re-upload task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,3 +3,8 @@ exec { 'fix--for-nginx':
   provider => 'shell',
   command  => "sed -i 's:ULIMIT=\"-n 15\"::g' /etc/default/nginx",
 }
+
+exec { 'Restart NGINX':
+  provider => 'shell',
+  command  => 'service nginx restart',
+}
